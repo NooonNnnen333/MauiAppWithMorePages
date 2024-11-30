d1c975540a89eb98200ea012e30d0685e5a5e027
NooonNnnen333 <egorvas05@gmail.com>
2024-11-30 22:40:19 +0500

    first commit

--- System/SystemPg111.cs
+++ System/SystemPg111.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Text;
 using System.Xml;
 using System;
+using System.Windows.Input;
 using Microsoft.Maui.Storage;
 
 namespace Povtorenie333.System;
@@ -16,51 +17,51 @@ public partial class SystemPg111 : ObservableObject
 {
     public SystemPg111()
     {
+        save = new AsyncRelayCommand(SaveNote); // Иницилизируем метод SaveNote для сохранения
+        load = new AsyncRelayCommand(OpenNote); // Иницилизируем метод OpenNote для открытия
         items = new ObservableCollection<string>(managementCompanySvo.names);
-
+        OpenNote();
     }
 
-    [ObservableProperty] 
+    [ObservableProperty]
     public ManagementCompany managementCompanySvo = new ManagementCompany(); // Управляющая коммпания из классов
 
-    [ObservableProperty] 
-    public int indexxx;
+    [ObservableProperty] public int indexxx;
 
-    [ObservableProperty] 
-    public string sredn;
-    
-    [ObservableProperty]  
-    public ObservableCollection<string> items;
-    
-    
-    [ObservableProperty] 
-    public string textofName;
-    
-    [ObservableProperty]
-    private string buildingsCount;
+    [ObservableProperty] public string sredn;
 
-    [ObservableProperty]
-    public int i = 5;
-    
+    [ObservableProperty] public ObservableCollection<string> items;
+
+
+    [ObservableProperty] public string textofName;
+
+    [ObservableProperty] private string buildingsCount;
+
+    [ObservableProperty] public int i = 5;
+
     [RelayCommand]
     async Task CrtHS(string text)
     {
         if (string.IsNullOrEmpty(text))
             return;
-        
-        Dictionary<string, object> parametros = new Dictionary<string, object>() {{"Text", text}, {"Rationsss", managementCompanySvo}};
-    
+
+        Dictionary<string, object> parametros = new Dictionary<string, object>()
+            { { "Text", text }, { "Rationsss", managementCompanySvo } };
+
+        SaveNote();
         await Shell.Current.GoToAsync(nameof(Povtorenie333.HouseCreatePage), parametros);
     }
-    
+
     [RelayCommand]
-    async Task CrtOB( string text )
+    async Task CrtOB(string text)
     {
         if (string.IsNullOrEmpty(textofName))
             return;
-        
-        Dictionary<string, object> parametros = new Dictionary<string, object>() {{"Text", text}, {"Rationsss", managementCompanySvo}};
-        
+
+        Dictionary<string, object> parametros = new Dictionary<string, object>()
+            { { "Text", text }, { "Rationsss", managementCompanySvo } };
+
+        SaveNote();
         await Shell.Current.GoToAsync(nameof(OfficeBuildCreatePage), parametros);
     }
 
@@ -71,27 +72,27 @@ public partial class SystemPg111 : ObservableObject
             return;
 
         int indexxx = managementCompanySvo.names.IndexOf(buildingName);
-        
-        
+
+
         managementCompanySvo.quarters.RemoveAt(indexxx);
         Items.Remove(buildingName);
-        
+
         Sredn = Convert.ToString(ManagementCompanySvo.average);
-        
+
         // Обновляем счётчик
         BuildingsCount = Convert.ToString(managementCompanySvo.quarters.Count);
     }
-    
+
     [RelayCommand]
     async Task Function(string Nobject)
     {
-        
+
         int index = items.IndexOf(Nobject);
         indexxx = index;
-        
-        Dictionary<string, object> parametros = new Dictionary<string, object>() {{"Indx", index}};
+
+        Dictionary<string, object> parametros = new Dictionary<string, object>() { { "Indx", index } };
         await Shell.Current.GoToAsync(nameof(DetailObjectPage), parametros);
-        
+
     }
 
     [RelayCommand]
@@ -106,30 +107,84 @@ public partial class SystemPg111 : ObservableObject
         }
     }
     
-    // Сохранение в формате JSON
+    
+    
+    /*Сохранения файла*/
+    
+    
     
     [ObservableProperty]
-    string fileName = Path.Combine(FileSystem.AppDataDirectory, "JsData.json");
-    
-    public void ddd()
-    {
-        customers = new List<string>() { "fjgtignt" };
-        var serializedData = JsonSerializer.Serialize(customers);
-        File.WriteAllText(fileName, serializedData);
-    }
+    private string text;
 
     [ObservableProperty]
-    public List<string> customers;
-    
-    [ObservableProperty] 
-    public string nnn;
-    
-    public void deserialize()
-    {
-        var rawData = File.ReadAllText(fileName);
-        customers = JsonSerializer.Deserialize<List<string>>(rawData);
-        nnn = customers[0];
-    }
+    private ICommand save;
 
+    private async Task SaveNote()
+    {
+        text = "";
+        foreach (var svoManagement in managementCompanySvo.names)
+        {
+            text += svoManagement;
+            text += " ";
+        }
+        
+        var path = FileSystem.Current.AppDataDirectory;
+        var fullPath = Path.Combine(path, "MyFile.txt");
+
+        File.WriteAllText(fullPath, text);
+
+        await Shell.Current.DisplayAlert("Saved!", $"Note has been saved!{path}", "OK");
+    }
+    
+    [ObservableProperty]
+    private ICommand load;
+    
+    private async Task OpenNote()
+    {
+        var path = FileSystem.Current.AppDataDirectory;
+        var fullPath = Path.Combine(path, "MyFile.txt");
+
+        // Проверяем, существует ли файл
+        if (!File.Exists(fullPath))
+        {
+            await Shell.Current.DisplayAlert("Error", "File not found.", "OK");
+            return;
+        }
+
+        try
+        {
+            string textFromFile = File.ReadAllText(fullPath); // Считываем содержимое файла
+
+
+            string[] lines = textFromFile.Split(" "); // Преобразуем текст в строку разделение(.Split), так как разделены адреса - пробелом 
+
+            if (lines.Length == 0)
+            {
+                await Shell.Current.DisplayAlert("Error", "File is empty.", "OK");
+                return;
+            }
+
+            // Добавляем строки в список
+            foreach (var line in lines)
+            {
+                if (line != "") 
+                    managementCompanySvo.AddQuarters(new House(line, 35, 11));  // Выводим из MyFile.txt "text" и добавляем дом с таким же именим, но с 35 жильцами и 11 комнатами
+            }
+
+            // Обновляем ObservableCollection для отображения в UI
+            Items.Clear();
+            foreach (var name in managementCompanySvo.names)
+            {
+                Items.Add(name);
+            }
+
+            await Shell.Current.DisplayAlert("Loaded!", "File has been loaded!", "OK");
+        }
+        catch (Exception ex)
+        {
+            await Shell.Current.DisplayAlert("Error", $"An error occurred: {ex.Message}", "OK");
+        }
+    }
+    
 }
 
--- obj/Debug/net8.0-ios/iossimulator-arm64/Povtorenie333.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-arm64/Povtorenie333.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Povtorenie333")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+07d978c401a3a65fd78e8936b6bd3f548e91a59e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Povtorenie333")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Povtorenie333")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
